e6178b5284cfccd9a1fe6a28ecb9929f1e98d4f5
Miguel Salles <160500308+devsallesm@users.noreply.github.com>
2025-08-15 22:41:13 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const cors = require('cors');
 
+
+
+
+
+
 // 2. Criar uma instância do aplicativo Express
 const app = express();
 app.use(cors({ origin: '*' }));
@@ -10,6 +15,14 @@ app.use(cors({ origin: '*' }));
 // Usamos process.env.PORT para compatibilidade com ambientes de hospedagem (como Heroku)
 // ou a porta 3000 como padrão se a variável de ambiente não estiver definida.
 const PORT = process.env.PORT || 3000;
+
+// --- Iniciar o Servidor ---
+
+// Faz o aplicativo Express começar a "escutar" por requisições na porta definida.
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Acesse: http://localhost:${PORT}/monstros`);
+});
 
 // --- Dados Temporários em Memória ---
 // Agora os monstros são carregados de um arquivo JSON externo.
@@ -21,14 +34,31 @@ const monstros = require('./monstros.json');
 // Quando alguém fizer uma requisição GET para a URL base + '/monstros'
 // (ex: http://localhost:3000/monstros), esta função será executada.
 app.get('/monstros', (req, res) => {
-    // Retorna a array de monstros como uma resposta JSON
-    res.json(monstros);
-});
 
-// --- Iniciar o Servidor ---
+    const tipoCriatura = req.query.tipo_criatura;
+    const pontosVidamax = req.query.pontos_vida_max;
+    const pontosVidamin = req.query.pontos_vida_min;
+    const buscaTexto = req.query.q;
+    
+    let result = monstros;
 
-// Faz o aplicativo Express começar a "escutar" por requisições na porta definida.
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-    console.log(`Acesse: http://localhost:${PORT}/monstros`);
+    if (tipoCriatura) {
+        result = result.filter(m => m.tipo_criatura == tipoCriatura );
+    }
+    if (pontosVidamax) {
+        result = result.filter(m => m.pontos_vida <= Number(pontosVidamax) );
+    }
+    if (pontosVidamin) {
+        result = result.filter(m => m.pontos_vida >= Number(pontosVidamin) );
+    }
+    if (buscaTexto) {
+        const texto = buscaTexto.toLowerCase();
+        result = result.filter(m =>
+        (m.nome && m.nome.toLowerCase().includes(texto)) ||
+        (m.descricao && m.descricao.toLowerCase().includes(texto)) 
+        )
+    }
+   
+
+    res.json(result);
 });
